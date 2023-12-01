0de9a6b13ee564f36f542f2dac3499869d069199
Rodrigo <55567123+rodrigodasilv@users.noreply.github.com>
2023-12-01 11:39:52 -0300

    Add files via upload

--- client/client.py
+++ client/client.py
@@ -21,18 +21,18 @@ while response:
 html.close()
 
 response = b''
-socketClient.send('png'.encode('utf-8')) #Solicita o arquivo html
+ready = b''
+socketClient.send('png'.encode('utf-8')) #Solicita o arquivo png
 png = open('servidor.png', 'wb')
 ready, _, _ = select.select([socketClient], [], [], 1)  # Use 1 segundo de timeout
 if ready:
     response = socketClient.recv(1024)
 while response:
     png.write(response)
-    ready, _, _ = select.select([socketClient], [], [], 3)  # Use 1 segundo de timeout
+    ready, _, _ = select.select([socketClient], [], [], 1)  # Use 1 segundo de timeout
     if not ready:
         break
     response = socketClient.recv(1024)
-
 png.close()
 
 socketClient.send('disconnect'.encode('utf-8')) #Solicita a desconexão
